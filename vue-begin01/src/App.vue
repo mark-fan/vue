ab126d0f9a7c93ec4b5e0c4ce5c980ecec1eb6be
luofanfan <15289340589@163.com>
2023-02-03 17:18:19 +0800

    使用vue2学习vue

--- vue-begin01/src/App.vue
+++ vue-begin01/src/App.vue
@@ -1,20 +1,23 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+// 声明式渲染 可以提高开发效率
+export default{
+  data:function(){
+    return{
+      num:1,
+      name:"luofan"
+    }
+  }
+}
 </script>
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <p>{{num}}</p>
+    <p>{{name}}</p>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
+<style>
 .logo {
   height: 6em;
   padding: 1.5em;
