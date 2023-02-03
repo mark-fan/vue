6c1b7cc93191458813da870fc07df6e8faa4f1a9
luofanfan <15289340589@163.com>
2023-02-03 18:16:12 +0800

    Update App.vue
    模板语法

--- vue-begin01/src/App.vue
+++ vue-begin01/src/App.vue
@@ -1,19 +1,38 @@
 <script>
 // 声明式渲染 可以提高开发效率
+// App.vue 根组件
 export default{
   data:function(){
     return{
       num:1,
-      name:"luofan"
+      name:"luofan",
+      msg:'<h2>标题</h2>',
+      id:'d1',
+      url:'https://img12.360buyimg.com/seckillcms/s140x140_jfs/t1/51326/23/21454/20614/63d8e210Ff06e6e55/5d859c31a6af098d.jpg.webp'
+    }
+  },
+  methods:{
+    changName:function(){
+      // this指向vue实例
+      this.name = 'luofanfan'
     }
   }
 }
 </script>
 
 <template>
+  <!-- 组件实例的所有property,不管怎么定义 都可以在组件的模板中访问 -->
   <div>
     <p>{{num}}</p>
     <p>{{name}}</p>
+    <!-- v-once：当我的数据发生变化，插值处数据不再变化 -->
+    <p v-once>{{name}}</p> 
+    <button @click="changName">改变名字</button>
+    <p>{{msg}}</p>
+    <!-- v-html:将我们的数据当作html显示 -->
+    <p v-html="msg"></p>
+    <p v-bind:id="id">v-bind绑定</p>
+    <img v-bind:src="url">
   </div>
 </template>
 
@@ -30,4 +49,5 @@ export default{
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
+
 </style>
