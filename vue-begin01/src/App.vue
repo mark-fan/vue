f3d8877fa493e55834ac3db6474db266c595f831
luofanfan <15289340589@163.com>
2023-02-03 10:20:51 +0800

    声明式渲染begin01 vue2

---
+++ vue-begin01/src/App.vue
@@ -0,0 +1,34 @@
+<script>
+//声明式渲染 可以提高开发效率
+export default{
+  data(){
+    return{
+      num:0,
+      name:"zhangsan"
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <p>{{num}}</p>
+    <p>{{name}}</p>
+  </div>
+  
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
