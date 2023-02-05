81a61933b0ba8651c098c54c8859106c548c4294
mark-fan <15289340589@163.com>
2023-02-05 13:24:46 +0800

    computed和methods区别 前者利用缓存 提高性能

--- vue-begin02/src/App.vue
+++ vue-begin02/src/App.vue
@@ -5,7 +5,21 @@ export default{
     message:"hello world",
     id:1
    }
+  },
+  // 计算属性  
+  computed:{ //只要依赖值不变  就不会重新计算 计算属性将基于他们的响应依赖关系缓存，提高性能
+    reverseMessage:function(){
+      console.log("计算属性");
+      return this.message.split(' ').reverse().join(' ')
+    }
+  },
+  methods:{
+    reverseMsg:function(){
+      console.log("methods");
+      return this.message.split('').reverse().join('')
+    }
   }
+
 }
 </script>
 
@@ -13,6 +27,20 @@ export default{
 
 <template>
   <p>{{message}}</p>
+  <!-- 第一种：使用js表达式 -->
+  <p>{{message.split(' ').reverse().join(' ')}}</p>
+  <p>{{message.split(' ').reverse().join(' ')}}</p>
+  <p>{{message.split(' ').reverse().join(' ')}}</p>
+  <!-- 第二种使用计算属性 -->
+  <p>{{reverseMessage}}</p>
+  <p>{{reverseMessage}}</p>
+  <p>{{reverseMessage}}</p>
+  <!-- 第三种 使用methods方法 -->
+  <p>{{reverseMsg()}}</p>
+  <p>{{reverseMsg()}}</p>
+  <p>{{reverseMsg()}}</p>
+  <button @click="message='nihao'">改变message</button>
+
 </template>
 
 <style scoped>
